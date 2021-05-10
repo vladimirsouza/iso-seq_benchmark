60d9ce2377956f7c4deca4dae276cc802b081bfe
vladimirsouza <vladimirbcs@gmail.com>
2021-05-10 20:52:11 +0200

    Add data.R file

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' The vlad_master_table_v3 master table
+#'
+#' A master table generated from Jurkat human cells. Variant callers to compare
+#' are DeepVariant and GATK on Iso-Seq data. The ground-truth was generated with
+#' GATK from short-read data.
+#'
+#' @docType data
+#'
+#' @usage data(vlad_master_table_v3)
+#'
+#' @format A data.frame with dimensions equal to (249059, 62).
+#'
+#' Each row is a variant called by at least one of the methods. For column
+#' descriptions, see \url{https://docs.google.com/document/d/1REIXRS6_oGEjTtlokwZ0pBQmoZcEE7L6Nkq-nCMoI5c/edit}
+"extractbams_output"
+
