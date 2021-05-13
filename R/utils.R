8e2ced2d32bb02d6242913898b50209abc5c1a8f
vladimirsouza <vladimirbcs@gmail.com>
2021-05-13 21:54:29 +0200

    Add function igv_batch_screenshots
    Create file R/utils.R. The function is added to it.

---
+++ R/utils.R
@@ -0,0 +1,49 @@
+#' Generate igv batch screenshots script
+#'
+#' @param chrm A vector of string. Chromossome name, e.g. "chrm1".
+#' @param pos A vector of integers. Positions to center the screenshot.
+#' @param output_dir A 1-length string. Directory where the IGV-batch-screenshot 
+#'   script is saved.
+#' @param prefix A 1-length string.
+#' @param snapshot_path A 1-length string. Directory where the IGV-batch-screenshot 
+#'   script will save the screenshots.
+#' @param windows_size 1-length integer. Window size of the screenshot.
+#' @param screenshot_number 1-length integer. Number of screenshot to generate.
+#'
+#' @return NULL
+#' 
+#' @importFrom IRanges IRanges resize start end
+#' @importFrom rlang .data
+#' @importFrom utils write.table
+#' @import dplyr
+#' 
+#' @export
+igv_batch_screenshots <- function(chrm, pos, output_dir, prefix, snapshot_path, windows_size=1501, screenshot_number=100) {
+  ### create the bed file that store the area of interest to visualize
+  bed <- IRanges(pos, width=1) %>% 
+    resize(windows_size, "center") 
+  bed <- data.frame(chrm=chrm, start=start(bed), end=end(bed))
+  
+  ### subset from bed
+  if( nrow(bed) < screenshot_number ) screenshot_number <- nrow
+  bed <- {1:nrow(bed)} %>% 
+    sample(screenshot_number) %>% 
+    bed[.data,]
+  
+  ### write bed file
+  bed_file <- file.path( output_dir, paste0(prefix, ".bed") )
+  write.table(bed,
+              bed_file,
+              sep="\t",
+              quote=FALSE,
+              row.names=FALSE,
+              col.names=FALSE)
+  
+  ### convert the bed file into an IGV batch script
+  batch_file <- file.path(output_dir, paste0(prefix, ".batch"))
+  cmds <- gettextf("bedtools igv -path %s -i %s > %s",
+                   file.path(snapshot_path, prefix), 
+                   bed_file, batch_file)
+  system(cmds)
+  unlink(bed_file)
+}
