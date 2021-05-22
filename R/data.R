88c123c77f59a9a7ef3d11852bddc81ac0fb8eb5
vladimirsouza <vladimirbcs@gmail.com>
2021-05-22 11:19:06 +0200

    Fix modification with jurkat_master_table_v4_notF

--- R/data.R
+++ R/data.R
@@ -1,21 +1,3 @@
-#' The vlad_master_table_v3 master table
-#'
-#' A master table generated from Jurkat human cells. Variant callers to compare
-#' are DeepVariant and GATK on Iso-Seq data. The ground-truth was generated with
-#' GATK from short-read data.
-#'
-#' @docType data
-#'
-#' @usage data(vlad_master_table_v3)
-#'
-#' @format A data.frame with dimensions equal to (249059, 62).
-#'
-#' Each row is a variant called by at least one of the methods. For column
-#' descriptions, see \url{https://docs.google.com/document/d/1REIXRS6_oGEjTtlokwZ0pBQmoZcEE7L6Nkq-nCMoI5c/edit}
-"vlad_master_table_v3"
-
-
-
 #' The jurkat_master_table_v4_notFiltered master table
 #'
 #' A master table generated from Jurkat human cells. Variant callers to compare
@@ -24,7 +6,7 @@
 #'
 #' @docType data
 #'
-#' @usage data(vlad_master_table_v3)
+#' @usage data(jurkat_master_table_v4_notFiltered)
 #'
 #' @format A data.frame with dimensions equal to (1020553, 30).
 #'
