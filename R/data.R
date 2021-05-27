ca080ada3a45a4089d4fb4918d85567e7c59acbc
vladimirsouza <vladimirbcs@gmail.com>
2021-05-27 15:26:50 +0200

    Updata Jurkat master table
    Columns about indels were added.

--- R/data.R
+++ R/data.R
@@ -1,4 +1,4 @@
-#' The jurkat_master_table_v4_notFiltered master table
+#' The jurkat_master_table_v5_notFiltered master table
 #'
 #' A master table generated from Jurkat human cells. Variant callers to compare
 #' are DeepVariant and GATK on Iso-Seq data. The ground-truth was generated with
@@ -6,11 +6,11 @@
 #'
 #' @docType data
 #'
-#' @usage data(jurkat_master_table_v4_notFiltered)
+#' @usage data(jurkat_master_table_v5_notFiltered)
 #'
-#' @format A data.frame with dimensions equal to (1020553, 30).
+#' @format A data.frame with dimensions equal to (1020553, 35).
 #'
 #' Each row is a variant called by at least one of the methods. For column
 #' descriptions, see \url{https://docs.google.com/document/d/1REIXRS6_oGEjTtlokwZ0pBQmoZcEE7L6Nkq-nCMoI5c/edit}
-"jurkat_master_table_v4_notFiltered"
+"jurkat_master_table_v5_notFiltered"
 
