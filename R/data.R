f51e2bdee362ed88d29500fd89f84a368183215b
vladimirsouza <vladimirbcs@gmail.com>
2021-06-16 20:00:45 +0200

    Add m. table jurkat_master_table_v7_notFiltered

--- R/data.R
+++ R/data.R
@@ -45,3 +45,27 @@
 #' descriptions, see \url{https://docs.google.com/document/d/1REIXRS6_oGEjTtlokwZ0pBQmoZcEE7L6Nkq-nCMoI5c/edit}
 "jurkat_master_table_v6_notFiltered"
 
+
+
+
+#' The jurkat_master_table_v7_notFiltered master table
+#'
+#' A master table generated from Jurkat human cells. Variant callers to compare
+#' are DeepVariant and GATK on Iso-Seq data. The ground-truth was generated with
+#' GATK from short-read data.
+#' 
+#' Each row is a variant called by at least one of the methods. For column
+#' descriptions, see \url{https://docs.google.com/document/d/1REIXRS6_oGEjTtlokwZ0pBQmoZcEE7L6Nkq-nCMoI5c/edit}
+#' 
+#' Methods to evaluate: dv_s_fc, gatk_s.
+#' 
+#' Ground-truth: tr_dna_merged.
+#' 
+#' No filtering applied.
+#' 
+#' @docType data
+#'
+#' @usage data(jurkat_master_table_v7_notFiltered)
+#'
+#' @format A data.frame with dimensions equal to (1018480, 31).
+"jurkat_master_table_v7_notFiltered"
